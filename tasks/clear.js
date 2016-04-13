bb807c525ece13d2324ce04ae019a84465e762ef
Truemedia <w.penistone@gmail.com>
2016-04-13 20:54:42 +0100

    Split up clear task into subtasks

--- tasks/clear.js
+++ tasks/clear.js
@@ -2,16 +2,31 @@ var gulp = require('gulp'),
 	gulpPlugins = require('auto-plug')('gulp');
 
 /* Clean up generated files (useful for development) */
-gulp.task('clear', function()
-{
-    return gulp.src([
-			'./app/Http/Controllers/*.php', // Controllers
-			'./database/migrations/*.php', // Migrations
-			'./app/*.php', // Models
-			'./app/Http/routes.php', // Routes
-			// Views
-			'./resources/views/**/**/*.php',
-			'./resources/views/**/**/*'
-		], { read: false })
-        .pipe( gulpPlugins.rm() )
+gulp.task('clear', ['clear-controllers', 'clear-migrations', 'clear-models', 'clear-routes', 'clear-views'], function() {
+    return;
+});
+
+// Clear controllers
+gulp.task('clear-controllers', function() {
+	return gulp.src(['./app/Http/Controllers/*.php'], { read: false }).pipe( gulpPlugins.rm() );
+});
+
+// Clear migrations
+gulp.task('clear-migrations', function() {
+	return gulp.src(['./database/migrations/*.php'], { read: false }).pipe( gulpPlugins.rm() );
+});
+
+// Clear models
+gulp.task('clear-models', function() {
+	return gulp.src(['./app/*.php'], { read: false }).pipe( gulpPlugins.rm() );
+});
+
+// Clear routes
+gulp.task('clear-routes', function() {
+	return gulp.src(['./app/Http/routes.php'], { read: false }).pipe( gulpPlugins.rm() );
+});
+
+// Clear views
+gulp.task('clear-views', function() {
+	return gulp.src(['./resources/views/**/**/*.php', './resources/views/**/**/*'], { read: false }).pipe( gulpPlugins.rm() );
 });
