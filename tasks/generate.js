9a6ad8d190aab22a7f6cec91bfaf163cd2d05a12
Truemedia <w.penistone@gmail.com>
2016-07-16 21:05:50 +0100

    Moved generate task questions to helper

--- tasks/generate.js
+++ tasks/generate.js
@@ -4,144 +4,25 @@ var gulp = require('gulp'),
     gulpPlugins = require('auto-plug')('gulp'),
     _ = require('underscore.string'),
     inquirer = require('inquirer'),
-    path = require('path'),
-    CountryLanguage = require('country-language');
+    path = require('path');
 
 // Helpers
 var schema = require('./../helpers/schema'),
+    questionaire = require('./../helpers/questionaire'),
     changeCase = require('change-case'),
     jsonfile = require('jsonfile'),
     walk = require('tree-walk');
 
 // Configs
-var defaults = require('./../config/defaults.json'),
-    questions = require('./../config/questions.json');
+var defaults = require('./../config/defaults.json');
 
 // CLI UI
 var ProgressBar = require('progress');
 
 gulp.task('generate', function(done)
 {
-    var prompts = [
-        {
-            name: 'installAgree',
-            message: 'This library will create and possibly overwrite files in your Laravel instance. This may not be reversable, do you wish to continue?',
-            type: 'confirm',
-            default: true
-        },
-        questions[0],
-                /* Not available yet */
-                // {
-                //     name: 'Build from specification (using regenerator.json)',
-                //     value: 'specification',
-                // },
-                /* Not available yet */
-                // {
-                //     name: 'Insane (Create everything and anything possible)',
-                //     value: 'insane',
-                // }
-        {
-            name: 'components',
-            message: 'What files would you like to generated based on the schemas you provided?',
-            type: 'checkbox',
-            choices: function()
-            {
-                var choices = [],
-                    components = [
-                        // 'Full package*',
-                        // 'Assets',
-                        'Configuration file',
-                        'Controller',
-                        'Command',
-                        // 'Event',
-                        // 'Middleware',
-                        'Handler',
-                        'Migration',
-                        'Model',
-                        'Policy',
-                        'Provider',
-                        'Request',
-                        'Routes',
-                        'Seed',
-                        'View',
-                        // 'Unit test',
-                    ];
-
-                components.forEach( function(name)
-                {
-                    var checked = false;
-                    choices.push({name, checked});
-                });
-
-                return choices;
-            }
-        },
-        {
-            name: 'locales',
-            message: 'Please select the locales your application is going to support',
-            type: 'checkbox',
-            choices: function()
-            {
-                var choices = [];
-                CountryLanguage.getLocales(true).forEach(function(locale)
-                {
-                    var choice = {'name': locale};
-                    choice.checked = (locale === defaults.locale);
-                    choices.push(choice);
-                });
-
-                return choices;
-            }
-        },
-        {
-            name: 'df',
-            message: 'How would you like your dates to be formatted?',
-            type: 'list',
-            choices: function(answers)
-            {
-                // TODO: Process all locales to pre-select using reliable data source of locale date formats
-                var has_locale_for_df = (answers.locales.indexOf('en-GB') > -1);
-
-                var choices = [
-                    {
-                        name: 'Default (Carbon)',
-                        value: 'default'
-                    },
-                    {
-                        name: 'British (en-GB)',
-                        value: 'british'
-                    }
-                ];
-
-                if (has_locale_for_df)
-                {
-                    choices.reverse();
-                }
-
-                return choices;
-            }
-        },
-        {
-            name: 'encoding',
-            message: 'What file encoding would you like to use?',
-            type: 'list',
-            choices: function(answers)
-            {
-                // TODO: Populate list of all file encoding options
-                var choices = [];
-
-                choices.push({
-                    name: changeCase.upperCase(defaults.encoding),
-                    value: defaults.encoding
-                });
-
-                return choices;
-            }
-        },
-        questions[1]
-    ];
-    //Ask
-    inquirer.prompt(prompts, function(answers)
+    // Ask
+    inquirer.prompt(questionaire.ask(), function(answers)
     {
         if (answers.installAgree)
         {
